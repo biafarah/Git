c7867d0a032d97f2a688d0bc85662c3e598439a9
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-23 21:47:31 -0200

    Bhaskara - commit all

---
+++ Bhaskara/fn13-exercicio4/fn13-exercicio4/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace fn13_exercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bhaskaraButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bhaskaraButton
+            // 
+            this.bhaskaraButton.Location = new System.Drawing.Point(129, 119);
+            this.bhaskaraButton.Name = "bhaskaraButton";
+            this.bhaskaraButton.Size = new System.Drawing.Size(121, 46);
+            this.bhaskaraButton.TabIndex = 0;
+            this.bhaskaraButton.Text = "Qual o valor?";
+            this.bhaskaraButton.UseVisualStyleBackColor = true;
+            this.bhaskaraButton.Click += new System.EventHandler(this.bhaskaraButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 320);
+            this.Controls.Add(this.bhaskaraButton);
+            this.Name = "Form1";
+            this.Text = "Bhaskara";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bhaskaraButton;
+    }
+}
+
---
+++ Bhaskara/fn13-exercicio4/fn13-exercicio4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bhaskaraButton_Click(object sender, EventArgs e)
+        {
+            double delta, a1, a2;
+            int a = 30, b = 100, c = 20;
+
+            delta = (b * b) - (4 * a * c);
+
+            a1 = (-b + (Math.Sqrt(delta))) / (2*a);
+            a2 = (-b - (Math.Sqrt(delta))) / (2 * a);
+
+            MessageBox.Show("A': " + a1 + "A'': " + a2);
+        }
+    }
+}
