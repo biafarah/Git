122abf34301d41478dd0105b9e27d7d4cb5bdf15
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-25 14:49:14 -0200

    Commit

---
+++ CalculoNotaFiscal/fn13-exercicio4.5/fn13-exercicio4.5/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace fn13_exercicio4._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.impostoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // impostoButton
+            // 
+            this.impostoButton.Location = new System.Drawing.Point(151, 107);
+            this.impostoButton.Name = "impostoButton";
+            this.impostoButton.Size = new System.Drawing.Size(124, 55);
+            this.impostoButton.TabIndex = 0;
+            this.impostoButton.Text = "Imposto";
+            this.impostoButton.UseVisualStyleBackColor = true;
+            this.impostoButton.Click += new System.EventHandler(this.impostoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 281);
+            this.Controls.Add(this.impostoButton);
+            this.Name = "Form1";
+            this.Text = "Qual o valor?";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button impostoButton;
+    }
+}
+
---
+++ CalculoNotaFiscal/fn13-exercicio4.5/fn13-exercicio4.5/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio4._5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void impostoButton_Click(object sender, EventArgs e)
+        {
+            double valorNotaFiscal = 2599.0;
+            double valorImposto = 0.0;
+
+            if (valorNotaFiscal <= 999)
+            {
+                valorImposto = valorNotaFiscal + (valorNotaFiscal * 0.02);
+            }
+            else if (valorNotaFiscal >= 1000 && valorNotaFiscal <= 2999)
+            {
+                valorImposto = valorNotaFiscal + (valorNotaFiscal * 0.025);
+            }
+            else if (valorNotaFiscal >= 3000 && valorNotaFiscal <= 6999)
+            {
+                valorImposto = valorNotaFiscal + (valorNotaFiscal * 0.028);
+            }
+            else if (valorNotaFiscal >= 7000)
+            {
+                valorImposto = valorNotaFiscal + (valorNotaFiscal * 0.03);
+            }
+
+            MessageBox.Show("O valor do imposto foi: " + valorImposto);
+        }
+    }
+}
