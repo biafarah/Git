b358e3ce06ea9b700ba4b876a637d6b7b9e361fc
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-24 22:27:43 -0200

    Votação - Commit All

---
+++ PodeVotar/fn13-exercicio4.4/fn13-exercicio4.4/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace fn13_exercicio4._4
+{
+    partial class VotacaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.votarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // votarButton
+            // 
+            this.votarButton.Location = new System.Drawing.Point(86, 82);
+            this.votarButton.Name = "votarButton";
+            this.votarButton.Size = new System.Drawing.Size(120, 51);
+            this.votarButton.TabIndex = 0;
+            this.votarButton.Text = "Pode Votar ?";
+            this.votarButton.UseVisualStyleBackColor = true;
+            this.votarButton.Click += new System.EventHandler(this.votarButton_Click);
+            // 
+            // VotacaoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 227);
+            this.Controls.Add(this.votarButton);
+            this.Name = "VotacaoForm";
+            this.Text = "Votação";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button votarButton;
+    }
+}
+
---
+++ PodeVotar/fn13-exercicio4.4/fn13-exercicio4.4/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio4._4
+{
+    public partial class VotacaoForm : Form
+    {
+        public VotacaoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void votarButton_Click(object sender, EventArgs e)
+        {
+            //Regra para votar é ser maior de 16 e brasileiro
+            int idade = 20;
+            bool isBrasileira = false;
+            string podeVotar = (idade >= 16 && isBrasileira) ?  "Pode Votar" : "Não Pode Votar !";
+
+            MessageBox.Show(podeVotar);
+        }
+    }
+}
