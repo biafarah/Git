25230f9386f3883574b302dd34767655e73a7334
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-28 20:55:08 -0200

    Commit

---
+++ SomaMultiplo3/fn13-exercicio5.3/fn13-exercicio5.3/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace fn13_exercicio5._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.somaButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // somaButton
+            // 
+            this.somaButton.Location = new System.Drawing.Point(140, 148);
+            this.somaButton.Name = "somaButton";
+            this.somaButton.Size = new System.Drawing.Size(141, 64);
+            this.somaButton.TabIndex = 0;
+            this.somaButton.Text = "Soma múltiplos 3";
+            this.somaButton.UseVisualStyleBackColor = true;
+            this.somaButton.Click += new System.EventHandler(this.somaButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 344);
+            this.Controls.Add(this.somaButton);
+            this.Name = "Form1";
+            this.Text = "Somatório II";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button somaButton;
+    }
+}
+
---
+++ SomaMultiplo3/fn13-exercicio5.3/fn13-exercicio5.3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void somaButton_Click(object sender, EventArgs e)
+        {
+            int soma = 0;
+            int contador = 1;
+
+            do
+            {
+                if (contador % 3 == 0)
+                {
+                    soma += contador;
+                }
+                contador++;
+            } while (contador <= 100);
+
+            MessageBox.Show("Somatório de 1 até 100: " + soma);
+        }
+    }
+}
