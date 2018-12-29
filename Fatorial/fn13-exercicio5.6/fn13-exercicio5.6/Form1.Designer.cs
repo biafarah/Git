5490352c5016b51438f4a4909c948ff1b8d540a4
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-28 22:50:34 -0200

    commit

---
+++ Fatorial/fn13-exercicio5.6/fn13-exercicio5.6/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace fn13_exercicio5._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fatorialButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fatorialButton
+            // 
+            this.fatorialButton.Location = new System.Drawing.Point(86, 77);
+            this.fatorialButton.Name = "fatorialButton";
+            this.fatorialButton.Size = new System.Drawing.Size(153, 59);
+            this.fatorialButton.TabIndex = 0;
+            this.fatorialButton.Text = "Fatorial 1 a 10";
+            this.fatorialButton.UseVisualStyleBackColor = true;
+            this.fatorialButton.Click += new System.EventHandler(this.fatorialButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 239);
+            this.Controls.Add(this.fatorialButton);
+            this.Name = "Form1";
+            this.Text = "Fatorial";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fatorialButton;
+    }
+}
+
---
+++ Fatorial/fn13-exercicio5.6/fn13-exercicio5.6/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio5._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fatorialButton_Click(object sender, EventArgs e)
+        {
+            int fatorial = 1;
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial *= n;
+                MessageBox.Show(n + " fatorial= " + fatorial);
+            }
+        }
+    }
+}
