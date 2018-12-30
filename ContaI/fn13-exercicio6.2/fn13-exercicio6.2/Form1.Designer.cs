c9e24b1b3cde3102e1643d36aef4e39fa340a449
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-30 11:14:37 -0200

    commit

---
+++ ContaI/fn13-exercicio6.2/fn13-exercicio6.2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace fn13_exercicio6._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contaButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contaButton
+            // 
+            this.contaButton.Location = new System.Drawing.Point(111, 91);
+            this.contaButton.Name = "contaButton";
+            this.contaButton.Size = new System.Drawing.Size(166, 64);
+            this.contaButton.TabIndex = 0;
+            this.contaButton.Text = "Conta";
+            this.contaButton.UseVisualStyleBackColor = true;
+            this.contaButton.Click += new System.EventHandler(this.contaButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 281);
+            this.Controls.Add(this.contaButton);
+            this.Name = "Form1";
+            this.Text = "ContaI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button contaButton;
+    }
+}
+
---
+++ ContaI/fn13-exercicio6.2/fn13-exercicio6.2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio6._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contaButton_Click(object sender, EventArgs e)
+        {
+            Conta contaJosefina = new Conta();
+            contaJosefina.titular = "Josefina";
+            contaJosefina.numero = 1;
+            contaJosefina.saldo = 100.0;
+            contaJosefina.Deposita(100.0);
+
+            MessageBox.Show("Titular: " +contaJosefina.titular);
+            MessageBox.Show("Saldo: " +contaJosefina.saldo.ToString());
+            contaJosefina.Saca(50.0);
+            MessageBox.Show("Saldo depois do saque: " + contaJosefina.saldo.ToString());
+
+
+            Conta contaMarcelino = new Conta();
+            contaMarcelino.titular = "Marcelino";
+            contaMarcelino.numero = 2;
+            contaMarcelino.saldo = 200.0;
+            contaMarcelino.Deposita(100.0);
+
+            MessageBox.Show("Titular: " + contaMarcelino.titular);            
+            MessageBox.Show("Saldo: " + contaMarcelino.saldo.ToString());
+            contaMarcelino.Saca(50.0);
+            MessageBox.Show("Saldo depois do saque: " + contaMarcelino.saldo.ToString());
+
+        }
+    }
+}
