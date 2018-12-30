03a0453cd11d7be4cb04d1bfbd59fee7290e99db
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-30 13:56:21 -0200

    commit

---
+++ ContaV/fn13-exercicio6.16/fn13-exercicio6.16/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fn13_exercicio6._16
+{
+    public class Cliente
+    {
+        public string nome;
+        public string rg;
+        public string cpf;
+        public string endereco;
+        public int idade;
+
+        public bool EhMaiorDeIdade()
+        {
+            if (this.idade >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;    
+            }
+        }
+    }
+}
---
+++ ContaV/fn13-exercicio6.16/fn13-exercicio6.16/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio6._16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contaButton_Click(object sender, EventArgs e)
+        {
+            Conta conta = new Conta();
+            conta.titular = new Cliente();
+            conta.titular.nome = "Jorge";
+            Cliente cli = new Cliente();
+            cli.idade = 20;
+            conta.titular.idade = cli.idade;
+          
+            if (conta.titular.EhMaiorDeIdade())
+            {
+                MessageBox.Show("É maior de idade !");
+            }
+            else
+            {
+                MessageBox.Show("É menor de idade");
+            }
+        }
+    }
+}
