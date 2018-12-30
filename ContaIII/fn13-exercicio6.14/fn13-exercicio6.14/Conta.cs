004e89bcf0f971f29a77d6f2964d1fd6830e616f
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-30 11:59:38 -0200

    commit

---
+++ ContaIII/fn13-exercicio6.14/fn13-exercicio6.14/Conta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fn13_exercicio6._14
+{
+    public class Conta
+    {
+        public double saldo;
+        public int numero;
+        public string titular;
+        //public short idade;
+
+        public void Deposita(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public string Saca(double valor, short idade)
+        {
+            //Caso o cliente seja menor de idade limitar o saque para 200,00
+            if (valor >= 200.0 && idade <= 17)
+            {
+                return "Saque não permitido para menores";
+                //if (this.saldo >= valor)
+                //{
+                //    this.saldo -= valor;
+                //    return true;
+                //}
+                //else
+                //{
+                //    return false;
+                //}
+            }
+
+            else
+            {
+                if (this.saldo >= valor)
+                {
+                    this.saldo -= valor;
+                    return "Saque realizado com sucesso";
+                }
+                else
+                {
+                    return "Saldo insuficiente";
+                }
+            }
+        }
+    }
+}
---
+++ ContaIII/fn13-exercicio6.14/fn13-exercicio6.14/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio6._14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contaButton_Click(object sender, EventArgs e)
+        {
+            Conta contaJosefina = new Conta();
+            contaJosefina.titular = "Josefina";
+            contaJosefina.saldo = 250.0;
+
+            string msg = contaJosefina.Saca(350.0, 19);
+            
+            MessageBox.Show(msg);
+            
+            
+        }
+    }
+}
