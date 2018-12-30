4dfd07f04cae3f1c7c1ad53891446f385bb02d68
Bianca Ferrari Farah <bia_farah@hotmail.com>
2018-12-30 11:27:25 -0200

    commit

---
+++ ContaII/fn13-exercicio6.12/fn13-exercicio6.12/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fn13_exercicio6._12
+{
+    public class Conta
+    {
+        public double saldo;
+        public int numero;
+        public string titular;
+
+        public void Deposita(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Saca(double valor)
+        {
+            if (this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ ContaII/fn13-exercicio6.12/fn13-exercicio6.12/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio6._12
+{
+    public partial class contaForm : Form
+    {
+        public contaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void contaButton_Click(object sender, EventArgs e)
+        {
+            Conta contaJosefina = new Conta();
+            contaJosefina.saldo = 10.0;
+            contaJosefina.titular = "Josefina";
+            
+            if (contaJosefina.Saca(100.0))
+            {
+                MessageBox.Show("Saque Realizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+    }
+}
