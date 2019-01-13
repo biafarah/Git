8c127f2261948fa8e2eb44bd8ff46aca8c22fb8a
Bianca Ferrari Farah <bia_farah@hotmail.com>
2019-01-13 21:34:40 -0200

    commit

---
+++ fn13-exercicio10.2/fn13-exercicio10.2/ContaCorrente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fn13_exercicio10._2
+{
+    public class ContaCorrente : Conta
+    {
+        public override void Saca(double valor)
+        {
+            base.Saca(valor - 0.05);
+        }
+
+        public override void Deposita(double valor)
+        {
+            base.Deposita(valor - 0.10);
+        }
+    }
+}
---
+++ fn13-exercicio10.2/fn13-exercicio10.2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fn13_exercicio10._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Conta conta;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.conta = new ContaCorrente();
+            conta.Deposita(100.0);
+
+            conta.Numero = 1;
+            Cliente cli = new Cliente("Maria");
+            conta.Titular = cli;
+
+            titularTextBox.Text = conta.Titular.Nome;
+            numeroTextBox.Text = Convert.ToString(conta.Numero);
+            saldoTextBox.Text = Convert.ToString(conta.Saldo);           
+        }
+
+        private void sacarButton_Click(object sender, EventArgs e)
+        {
+            string valorDigitado = valorTextBox.Text;
+            double valorOperacao = Convert.ToDouble(valorDigitado);
+            Conta conta = new ContaCorrente();
+            conta.Saca(valorOperacao);
+            saldoTextBox.Text = Convert.ToString(conta.Saldo);
+        }
+    }
+}
