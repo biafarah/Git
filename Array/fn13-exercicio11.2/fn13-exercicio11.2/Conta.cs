9085fc7fe557fd6ae4b257c262ee59e341220de3
Bianca Ferrari Farah <bia_farah@hotmail.com>
2019-03-24 21:14:40 -0300

    commit

---
+++ Array/fn13-exercicio11.2/fn13-exercicio11.2/Conta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fn13_exercicio11._2
+{
+    public class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; private set; }
+        public Cliente Titular { get; set; }
+
+        public virtual void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public virtual void Saca(double valor)
+        {
+            this.Saldo -= valor;
+        }
+    }
+}
